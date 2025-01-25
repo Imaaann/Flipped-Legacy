3cc7a40559b6e65639e68b20ee33cb08417f6c7e
Imaaann <iman555bpe147@gmail.com>
2025-01-25 21:59:31 +0100

    Made Types for character serialization

---
+++ include/fl_types.h
@@ -0,0 +1,164 @@
+#pragma once
+
+enum _FLQuality { COMMON, UNCOMMON, RARE, EPIC, LEGENDARY };
+typedef enum _FLQuality FLQuality;
+
+enum _FLRegen { AUTO, OFFENSIVE, DEFENSIVE };
+typedef enum _FLRegen FLRegen;
+
+enum _FLCardClass { NORMAL, INCANTATION, ULTIMATE };
+typedef enum _FLCardClass FLCardClass;
+
+enum _FLCardType { ATK, HEAL, BUFF, DEBUFF, COUNTER };
+typedef enum _FLCardType FLCardType;
+
+enum _FLStat { MAX_HEALTH, HEALTH, ATTACK, DEFENSE, RESISTANCE, SPEED, ENERGY };
+typedef enum _FLStat FLStat;
+
+enum _FLPowerWordType { DEAL, HEAL, INFLICT, DISPEL, IMMUNE, SUMMON };
+typedef enum _FLPowerWordType FLPowerWordType;
+
+enum _FLDamageType { PHYSICAL, MAGICAL };
+typedef enum _FLDamageType FLDamageType;
+
+enum _FLConditionType {
+    NONE,
+    UNDER_HP,
+    ABOVE_HP,
+    ALLY_UNDER_HP,
+    ALLY_ABOVE_HP,
+    ENEMY_UNDER_HP,
+    ENEMY_ABOVE_HP,
+    NUMBER_ALLY_UNDER_HP,
+    NUMBER_ALLY_ABOVE_HP,
+    NUMBER_ENEMY_UNDER_HP,
+    NUMBER_ENEMY_ABOVE_HP,
+    NUMBER_ALLY_ALIVE,
+    NUMBER_ENEMY_ALIVE,
+    IS_BUFF,
+    IS_DEBUFF,
+    TARGET_BUFF,
+    TARGET_DEBUFF,
+    DAMAGE,
+    DAMAGE_UNDER_HP,
+    DAMAGE_ABOVE_HP,
+    DAMAGE_NUMBER_TURNS
+};
+typedef enum _FLConditionType FLConditionType;
+
+enum _FLEffectType {
+    // BUFFS
+    STATS_UP,
+    STATS_UP_STACK,
+    ROLL,
+    REFLECT,
+    ENERGY_SIPHON,
+    INVISIBILITY,
+    BARRIER,
+    INSPIRATION,
+    ARCANE_STAMP,
+
+    // DEBUFFS
+    STATS_DOWN,
+    STATS_DOWN_STACK,
+    NASTY_WOUND,
+    SEALED,
+    SILENCE,
+    TAUNT,
+    DISARM,
+    BIND,
+    PARALYZED,
+    SKILL_DRAIN,
+    DISCONCERT,
+    CONFUSED
+};
+typedef enum _FLEffectType FLEffectType;
+
+struct _FLStatSheet {
+    double hp;
+    double hpg; // g stands for growth
+    double atk;
+    double atkg;
+    double def;
+    double defg;
+    double res;
+    double resg;
+    double spd;
+    FLQuality quality;
+    FLRegen regen;
+};
+typedef struct _FLStatSheet FLStatSheet;
+
+typedef struct {
+    char gameName[64];
+    int characterCount;
+    int enemyCount;
+} FLGameData;
+
+struct _FLEffect {
+    FLEffectType type;
+    int coeff1;
+    int coeff2;
+    FLStat stat1;
+    FLStat stat2;
+};
+typedef struct _FLEffect FLEffect;
+
+struct _FLCondition {
+    FLConditionType type;
+    int coeff1;
+    int coeff2;
+    FLEffect effect;
+};
+typedef struct _FLCondition FLCondition;
+
+struct _FLPowerWord {
+    FLPowerWordType type;
+    int target;
+    FLStat stat;
+    FLEffect effect;
+    int length;
+    FLDamageType dmgType;
+};
+typedef struct _FLPowerWord FLPowerWord;
+
+struct _FLWord {
+    FLCondition condition;
+    FLPowerWord word;
+};
+typedef struct _FLWord FLWord;
+
+struct _FLCard {
+    FLCardClass class;
+    FLCardType type;
+    FLWord words[2];
+};
+typedef struct _FLCard FLCard;
+
+struct _FLUltimateCard {
+    FLCardClass class;
+    FLCardType type;
+    int energyCost;
+    FLWord words[3];
+};
+typedef struct _FLUltimateCard FLUltimateCard;
+
+struct _FLCardFamily {
+    FLCard members[3];
+};
+typedef struct _FLCardFamily FLCardFamily;
+
+struct _FLCharacter {
+    // Meta Data
+    char name[128];
+    char image[128];
+    char* description;
+
+    // StatSheet
+    FLStatSheet stats;
+
+    // Deck
+    FLCardFamily deck[8];
+    FLUltimateCard ultimate;
+};
+typedef struct _FLCharacter FLCharacter;
--- src/main.c
+++ src/main.c
@@ -1,16 +1,85 @@
+#include <fl_types.h>
 #include <ncurses/ncurses.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define FL_EDITOR_VERSION "beta-1.0"
+
+WINDOW* print_info(void) {
+    refresh();
+
+    int maxColumns = getmaxx(stdscr);
+    WINDOW* info = newwin(4, maxColumns, 0, 0);
+    box(info, 0, 0);
+
+    mvwprintw(info, 1, 1, "Welcome to the FL Editor");
+    mvwprintw(info, 2, 1, "Version: ");
+
+    wattron(info, COLOR_PAIR(1));
+    wprintw(info, "%s", FL_EDITOR_VERSION);
+    wattroff(info, COLOR_PAIR(1));
+    wrefresh(info);
+    return info;
+}
+
+FLGameData get_basic_info(void) {
+    refresh();
+
+    FLGameData data;
+
+    int maxColumns = getmaxx(stdscr);
+    WINDOW* input = newwin(5, maxColumns, 5, 0);
+    box(input, 0, 0);
+
+    mvwprintw(input, 1, 1, "Game Name: ");
+    mvwprintw(input, 2, 1, "Character Count: ");
+    mvwprintw(input, 3, 1, "Enemy Count: ");
+
+    char Buffer[256];
+    mvwscanw(input, 1, 12, "%[^\n]s", Buffer);
+    if (strlen(Buffer) > 63)
+        exit(1);
+    strncpy(data.gameName, Buffer, strlen(Buffer));
+
+    mvwscanw(input, 2, 18, "%d", &data.characterCount);
+    mvwscanw(input, 3, 14, "%d", &data.enemyCount);
+    wrefresh(input);
+    delwin(input);
+    return data;
+}
+
+FLGameData print_basic_info(FLGameData data) {
+    refresh();
+
+    int maxColumns = getmaxx(stdscr);
+    WINDOW* input = newwin(5, maxColumns, 5, 0);
+    box(input, 0, 0);
+
+    wattron(input, COLOR_PAIR(1));
+    mvwprintw(input, 1, 1, "Game Name: %s", data.gameName);
+    mvwprintw(input, 2, 1, "Character Count: %d", data.characterCount);
+    mvwprintw(input, 3, 1, "Enemy Count: %d", data.enemyCount);
+    wattroff(input, COLOR_PAIR(1));
+    wrefresh(input);
+
+    delwin(input);
+    return data;
+}
 
 int main() {
     initscr();
     start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
-    attron(COLOR_PAIR(1));
-    printw("Test");
-    attroff(COLOR_PAIR(1));
+    WINDOW* info = print_info();
 
-    refresh();
+    FLGameData data = get_basic_info();
+    print_basic_info(data);
+
+    for (int i = 0; i < data.characterCount; i++) {
+    }
+
+    delwin(info);
     getch();
     endwin();
 
